afed5f391a23472e8f53cf012065ea700390227e
Guido Siverio <siverioguido@gmail.com>
2024-10-09 10:28:28 -0300

    Subo modificacion en userController

--- src/main/java/frba/utn/edu/ar/tp_dds/controllers/UserController.java
+++ src/main/java/frba/utn/edu/ar/tp_dds/controllers/UserController.java
@@ -3,16 +3,17 @@ package frba.utn.edu.ar.tp_dds.controllers;
 import frba.utn.edu.ar.tp_dds.entities.User;
 import frba.utn.edu.ar.tp_dds.responses.LoginResponse;
 import frba.utn.edu.ar.tp_dds.services.UserService;
+import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin("*")
 public class UserController {
-
   private final UserService userService;
 
   public UserController(UserService userService) {
